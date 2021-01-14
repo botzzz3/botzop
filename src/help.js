04d7c101f61edec6bba609c16b8d92906380913e
botzzz3 <77111954+botzzz3@users.noreply.github.com>
2021-01-14 19:37:40 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,66 @@
+const help = (prefix) => {
+	return `.......L-BOT
+	
+	                
+┏━━━°❀ ❬ 𝘼𝘽𝙊𝙐𝙏 ❭ ❀°━━━┓
+┃
+┏❉ *${prefix}owner*
+┣❉ *${prefix}donasi*
+┗❉ *${prefix}creator*
+┃
+┣━━━°❀ ❬ 𝗠𝗔𝗞𝗘𝗥 ❭ ❀°━━━⊱
+┃
+┣➥ *${prefix}sticker* [foto]
+┣➥ *${prefix}stickergif* [foto]
+┣➥ *${prefix}sticker nobg 
+┣➥ *${prefix}thunder* [teks]
+┣➥ *${prefix}tsticker* [teks/url]
+┃
+┣━━━━°❀ ❬ 𝙈𝙀𝘿𝙄𝘼 ❭ ❀°━━━⊱
+┃
+┣➥ *${prefix}tts* [teks]
+┣➥ *${prefix}ocr*
+┣➥ *${prefix}loli*
+┣➥ *${prefix}toimg*
+┣➥ *${prefix}meme*
+┣➥ *${prefix}memeindo*
+┣➥ *${prefix}nsfwloli*
+┣➥ *${prefix}wait* [foto]
+┣➥ *${prefix}simi
+┣➥ *${prefix}simih*
+┣➥ *${prefix}wait*
+┃
+┣━━━━°❀ ❬ 𝙂𝙍𝙊𝙐𝙋 ❭ ❀°━━━━⊱
+┃
+┣➥ *${prefix}setname*
+┣➥ *${prefix}setdesc*
+┣➥ *${prefix}getpp*
+┣➥ *${prefix}tagall*
+┣➥ *${prefix}linkgroup
+┣➥ *${prefix}gprofile*
+┣➥ *${prefix}setprefix
+┣➥ *${prefix}welcome*
+┣➥ *${prefix}left*
+┃
+┣━━━━°❀ ❬ 𝙎𝙊𝙐𝙉𝘿 ❭ ❀°━━━━━⊱
+┃
+┣➥ *salam*
+┣➥ *tariksis*
+┣➥ *baka*
+┣➥ *desah*
+┣➥ *goblok*
+┣➥ *roti*
+┣➥ *welot*
+┣➥ *abangjago*
+┃
+┣━━━━━━━━━━━━━━━━━━━━
+┃𝐂𝐎𝐌𝐀𝐍𝐃𝐎𝐒 𝐀𝐋𝐈𝐀𝐃𝐎𝐒: 
+👑𝐇𝐂𝐁𝐑-𝐇𝐒𝐁𝐑-𝐇𝐊𝐁𝐑-𝐁𝐄𝐒𝐓-𝐋𝐔𝐍𝐀 👑
+┣━━━━━━━━━━━━━━━━━━━━
+┃ 🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿
+┗━━━━━━━━━━━━━━━━━━━━`
+}
+
+exports.help = help
+
+
